0547ee41b327f9f77744b958867137e3f92b1be9
HidanYugakure <fidants@code.edu.az>
2023-11-12 14:50:55 +0400

    Refactory Completed Written Calculator with Switch Expression and Written Reversed String methid with StringBuilder all other methods are available

--- 11.11.2023/Hidanin Modelsleri/Calculator_Class.cs
+++ 11.11.2023/Hidanin Modelsleri/Calculator_Class.cs
@@ -1,47 +1,65 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace tapsiriq_11._11._2023.Hidanin_Modelsleri
-//{
-//    class Calculator_Class
-//    {
-//        public void Calculation(double num1, double num2)
-//        {
-//            Console.WriteLine("Reqemi daxil edin (+, -, *, /): ");
-//            char reqem = Convert.ToChar(Console.ReadLine());
-//            double result = 0;
+namespace tapsiriq_11._11._2023.Hidanin_Modelsleri
+{
+    class Calculator_Class
+    {
+        //Metod 1 switch ile yazilib*/
+        //    public void Calculation(double num1, double num2)
+        //    {
+        //        Console.WriteLine("Reqemi daxil edin (+, -, *, /): ");
+        //        char reqem = Convert.ToChar(Console.ReadLine());
+        //        double result = 0;
 
-//            switch (reqem)
-//            {
-//                case '+':
-//                    result = num1 + num2;
-//                    break;
-//                case '-':
-//                    result = num1 - num2;
-//                    break;
-//                case '*':
-//                    result = num1 * num2;
-//                    break;
-//                case '/':
-//                    if (num2 != 0)
-//                    {
-//                        result = num1 / num2;
-//                    }
-//                    else
-//                    {
-//                        Console.WriteLine("riyaziyyat biliyime gore 0 ra bolmek mumkun deyil");
-//                        return;
-//                    }
-//                    break;
-//                default:
-//                    Console.WriteLine("düzgün operator daxil et (+, -, *, /).");
-//                    return;
-//            }
+        //        switch (reqem)
+        //        {
+        //            case '+':
+        //                result = num1 + num2;
+        //                break;
+        //            case '-':
+        //                result = num1 - num2;
+        //                break;
+        //            case '*':
+        //                result = num1 * num2;
+        //                break;
+        //            case '/':
+        //                if (num2 != 0)
+        //                {
+        //                    result = num1 / num2;
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine("riyaziyyat biliyime gore 0 ra bolmek mumkun deyil");
+        //                    return;
+        //                }
+        //                break;
+        //            default:
+        //                Console.WriteLine("düzgün operator daxil et (+, -, *, /).");
+        //                return;
+        //        }
 
-//            Console.WriteLine($"netice: {result}");
-//        }
-//    }
-//}
+        //        Console.WriteLine($"netice: {result}");
+        //    }
+        //
+
+        public void Calculate(int num1, int num2)
+        {
+            Console.WriteLine("Hesablama emeloyyati ucun operator daxil edin...");
+            char sign = Convert.ToChar(Console.ReadLine());
+
+            var result = sign switch
+            {
+                '+' => num1+num2,
+                '-' => num1-num2,
+                '*' => num1*num2,
+                '/' => num1/num2, //basqa bir elaveniz varsa comment yazarsiniz  zehmet olmasa Mr.Teacher Ulvi.
+            };
+
+            Console.WriteLine(result);
+        }
+    }
+}
--- 11.11.2023/Program.cs
+++ 11.11.2023/Program.cs
@@ -1,6 +1,20 @@
 ﻿#region 1 ci task
+using System.Text;
+using tapsiriq_11._11._2023.Hidanin_Modelsleri;
+
+Console.WriteLine("Ters cevirmek ucun string daxil edin...");
+string givenString = Console.ReadLine();
+StringBuilder reversedString = new();
+
+for (int i = givenString.Length - 1; i >= 0; i--)
+{
+    reversedString.Append(givenString[i]);
+    
+}
+Console.WriteLine(reversedString);
+
 //Console.WriteLine(ChangeWord("Salam.Necesen?"));
-//string ChangeWord(string word)
+//string ChangeWord(string word) //1-ci variant reversded string String buildersiz
 //{
 //    string result = string.Empty;
 //    for (int i = word.Length - 1; i >= 0; i--)
@@ -12,27 +26,25 @@
 //}
 #endregion
 #region 2 ci task
-//using tapsiriq_11._11._2023.Hidanin_Modelsleri;
-//Account account = new Account();
-//Console.WriteLine("sisteme daxil olmaq ucun username daxil et");
-//string Enterusername = Console.ReadLine();
-//Console.WriteLine("sisteme daxil olmaq ucun kodu daxil et");
-//string Enterpassword = Console.ReadLine();
-//account.Login(Enterusername, Enterpassword);
+Account account = new Account();
+Console.WriteLine("sisteme daxil olmaq ucun username daxil et");
+string Enterusername = Console.ReadLine();
+Console.WriteLine("sisteme daxil olmaq ucun kodu daxil et");
+string Enterpassword = Console.ReadLine();
+account.Login(Enterusername, Enterpassword);
 #endregion
 #region 3 cu task
-//using tapsiriq_11._11._2023.Hidanin_Modelsleri;
-//Factorial factorial = new Factorial();
-//Console.WriteLine("Istediyin ededi daxil ede bilersen");
-//int num = int.Parse(Console.ReadLine());
-//factorial.FactorialHesablanmasi(num);
+Factorial factorial = new Factorial();
+Console.WriteLine("Istediyin ededi daxil ede bilersen");
+int num = int.Parse(Console.ReadLine());
+factorial.FactorialHesablanmasi(num);
 #endregion
 #region 4 cu task
-//using tapsiriq_11._11._2023.Hidanin_Modelsleri;
-//Calculator_Class calculator_Class = new Calculator_Class();
-//Console.WriteLine("Birinci rəqəmi daxil edin: ");
-//double num1 = Convert.ToDouble(Console.ReadLine()); //doublacevirmekucun
-//Console.WriteLine("İkinci rəqəmi daxil edin: ");
-//double num2 = Convert.ToDouble(Console.ReadLine());//doublacevirmekucun
-//calculator_Class.Calculation(num1, num2);
+Calculator_Class calculator_Class = new Calculator_Class();
+Console.WriteLine("Birinci rəqəmi daxil edin: ");
+double num1 = Convert.ToDouble(Console.ReadLine()); //doublacevirmekucun
+Console.WriteLine("İkinci rəqəmi daxil edin: ");
+double num2 = Convert.ToDouble(Console.ReadLine());//doublacevirmekucun
+Calculator_Class calculation = new();
+calculation.Calculate(5, 4);
 #endregion
